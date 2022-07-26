097113caad810f7c3c7875a09cba9fedc52e77f1
Razee Hussein-Jamal <razee.hj@gmail.com>
2022-07-26 23:03:41 +0200

    Implements ´findAverage´ function

--- Sliding Window/Sliding Window.playground/Contents.swift
+++ Sliding Window/Sliding Window.playground/Contents.swift
@@ -1,10 +1,34 @@
 import UIKit
 
-/// Problem
-/// Maximum Sum Subarray of Size K (easy)
-///
 /// Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
 ///
+/// Given an array, find the average of all  subarrays of ‘K’ contiguous elements in it.
+/// Input: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+/// Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+
+func findAverage(k: Int, arr: [Int]) -> [Double] {
+    var windowStart = 0
+    var result = Array(repeating: 0.0, count: arr.count-k+1)
+    var sum = 0
+    
+    for windowEnd in 0..<arr.count {
+        sum += arr[windowEnd]
+        
+        if (windowEnd >= k-1) {
+            let avg = Double(sum)/Double(k)
+            result[windowStart] = avg
+            sum -= arr[windowStart]
+            windowStart += 1
+        }
+    }
+    
+    return result
+}
+
+findAverage(k: 5, arr: [1, 3, 2, 6, -1, 4, 1, 8, 2])
+
+/// Problem
+/// Maximum Sum Subarray of Size K (easy)
 /// Input: [2, 1, 5, 1, 3, 2], k=3
 /// Output: 9
 /// Explanation: Subarray with maximum sum is [5, 1, 3].
